074779299452ead20b6aeb5015e11f90fbd1a3a3
Kizito Chukwuma <kizitoweb3@gmail.com>
2026-01-09 16:59:41 +0100

    Add main.jsx to render RecruitmentPage component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RecruitmentPage from './RecruitmentPage.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RecruitmentPage />
+  </React.StrictMode>,
+);
